60759a3cf25af99ac83be6f2a1fed6e011d6a9c4
Gasana.NHonore <ndayiho5@gmail.com>
2021-03-01 21:11:59 +0100

    ajouter des parties suite à une condition..

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import Banner from './Banner';
 import Cart from './Cart';
+import ShoppingList from './ShoppingList';
 
 
 function App() {
   return (
-    <>
+    <div>
       <Banner />
-      <Cart />
-    </>
+      {/* <Cart /> */}
+      <ShoppingList/>
+    </div>
   )
 }
 
