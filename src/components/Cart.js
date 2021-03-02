749d25de40f85887244a7954769e3f32658616f2
Gasana.NHonore <ndayiho5@gmail.com>
2021-03-02 22:53:40 +0100

    Un hook est une fonction qui permet de « se brancher » (to hook up) sur des fonctionnalités React. ---> useState

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,19 +1,27 @@
 import '../styles/Cart.css'
+import { useState } from 'react'
 
 function Cart() {
     const monsteraPrice = 8
-    const ivyPrice = 10
-    const flowerPrice = 15
-    return (
-        <div className="lmj-cart">
+    const [cart, updateCart] = useState(0)
+    const [isOpen, setIsOpen] = useState(false)
+
+    return isOpen ? (
+        <div className='lmj-cart'>
+            <button onClick={() => setIsOpen(false)}>Fermer</button>
             <h2>Panier</h2>
-            <ul>
-                <li>Monstera : {monsteraPrice}€</li>
-                <li>Lierre : {ivyPrice}€</li>
-                <li>Fleurs : {flowerPrice}€</li>
-            </ul>
-          Total : {monsteraPrice + ivyPrice + flowerPrice}€
-        </div>)
+            <div>
+                Monstera : {monsteraPrice}€
+                <button onClick={() => updateCart(cart + 1)}>
+                    Ajouter
+                </button>
+            </div>
+            <h3>Total : {monsteraPrice * cart}€</h3>
+            <button onClick={() => updateCart(0)}>vider le panier</button>
+        </div>
+    ) : (
+            <button onClick={() => setIsOpen(true)}>Ouvrir le Panier</button>
+        )
 }
 
 export default Cart
