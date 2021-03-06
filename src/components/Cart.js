e3421505c67ac5a0082e650a1191839ac7aad520
Gasana.NHonore <ndayiho5@gmail.com>
2021-03-06 22:46:30 +0100

    useEffect introduction

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import '../styles/Cart.css'
+
+
 
 function Cart({ cart, updateCart }) {
 	const [isOpen, setIsOpen] = useState(true)
 	const total = cart.reduce(
-		(acc, plantType) => acc + plantType.amount * plantType.price,0
+		(acc, plantType) => acc + plantType.amount * plantType.price,
+		0
 	)
+
+		//on va appeler ça toujours quand le composant est re-render
+	// useEffect(() => {
+	// 	document.title = `Ndaho Maison de Jungle`
+	// })
+
+	//on va appeler ça uniquement la premiere fois
+	useEffect(() => {
+		document.title = `Ndaho Maison de Jungle`
+	}, [])
+
+	//on va appeler ça pour la premiere fois et quand le total change
+	// useEffect(() => {
+	// 	document.title = `LMJ: ${total}€ d'achats`
+	// }, [total])
+
+	//on va appeler ça uniquement quand le composant est demonté( avec un return)
+	useEffect(() => {
+		return console.log(`Notre cart est demonté/caché`);
+	})
+
+
 	return isOpen ? (
 		<div className='lmj-cart'>
 			<button
@@ -13,7 +38,7 @@ function Cart({ cart, updateCart }) {
 				onClick={() => setIsOpen(false)}
 			>
 				Fermer
-			</button>
+            </button>
 			{cart.length > 0 ? (
 				<div>
 					<h2>Panier</h2>
@@ -28,19 +53,19 @@ function Cart({ cart, updateCart }) {
 					<button onClick={() => updateCart([])}>Vider le panier</button>
 				</div>
 			) : (
-				<div>Votre panier est vide</div>
-			)}
+					<div>Votre panier est vide</div>
+				)}
 		</div>
 	) : (
-		<div className='lmj-cart-closed'>
-			<button
-				className='lmj-cart-toggle-button'
-				onClick={() => setIsOpen(true)}
-			>
-				Ouvrir le Panier
-			</button>
-		</div>
-	)
+			<div className='lmj-cart-closed'>
+				<button
+					className='lmj-cart-toggle-button'
+					onClick={() => setIsOpen(true)}
+				>
+					Ouvrir le Panier
+            </button>
+			</div>
+		)
 }
 
 export default Cart
